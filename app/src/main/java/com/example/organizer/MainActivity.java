6969a2bce1a654ba5ce2052002bab239dab74841
LukM88 <58972045+LukM88@users.noreply.github.com>
2020-03-31 21:45:11 +0200

    dodanie obsługi hasła
    dodanie pliku json oraz obsługi hasła

--- app/src/main/java/com/example/organizer/MainActivity.java
+++ app/src/main/java/com/example/organizer/MainActivity.java
@@ -2,17 +2,33 @@ package com.example.organizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.PasswordAuthentication;
 
 public class MainActivity extends AppCompatActivity {
-    DBHelper myDB;
     private Button nextButton;
     private TextView titleText;
+    private EditText password;
 
     @Override
     protected void onResume() {
@@ -25,17 +41,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hideNavigationBar();
-        myDB= new DBHelper(this);
+        // myDB= new DBHelper(this);
 
-
+        password =(EditText) findViewById(R.id.editText);
         nextButton = findViewById(R.id.nextButton);
         titleText = findViewById(R.id.titleText);
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), CalendarActivity.class);
-                startActivity(intent);
+                String pwd = null;
+
+                String json="";
+                try {
+                    InputStream is = getAssets().open("data");
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+                    String line = "";
+                    while (line!=null){
+                        line=bufferedReader.readLine();
+                        json= json + line;
+                    }
+
+                    JSONArray jasonArray = new JSONArray(json);
+                    JSONObject jasonObject = jasonArray.getJSONObject(0);
+                    pwd = jasonObject.getString("password");
+                    if(password.getText().toString().equals(pwd)) {
+                        Intent intent = new Intent(getBaseContext(), CalendarActivity.class);
+                        startActivity(intent);
+                    }else {
+                        Toast.makeText(getBaseContext(),"błędne hasło",Toast.LENGTH_LONG).show();
+                    }
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getBaseContext(), "błąd pliku", Toast.LENGTH_LONG).show();
+
+
+                }
+
             }
         });
 
